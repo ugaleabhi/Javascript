01247134b40e689c60642a8c0e38293b3c6b0e93
ugaleabhi <ugaleabhishek34@gmail.com>
2025-03-12 11:18:02 +0530

    new file add

---
+++ index3.js
@@ -0,0 +1,14 @@
+console.log("javascript lecture3: var,let,const");
+// var a=45;   // as soon as possible dont use var
+let b="Harry"
+let c=null
+let d=undefined;
+const author="Harry"
+//let author=5 Throws an error because constant cannot be changed
+b=4;
+const harry=0;
+{
+    let b="this"
+    console.log(b);
+}
+console.log(b);
