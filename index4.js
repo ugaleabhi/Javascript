3262f4530eadfa0c3d53521fcae6d25682f814f0
ugaleabhi <ugaleabhishek34@gmail.com>
2025-03-13 18:42:43 +0530

    update file

---
+++ index4.js
@@ -0,0 +1,22 @@
+//primitive datatypes
+//nn bb ss u
+let a=null;
+let b=345;
+let c=true;//can also be false
+let d=BigInt("567")+BigInt("5");
+let e="Harry"
+let f=Symbol("I am b nice symbol")
+let g=undefined;
+console.log(a,b,c,d,c,e,f,g);
+console.log(typeof d)
+
+
+//object non primitive datatypes
+const item=
+{
+    "harry":true,
+    "abhi":false,
+    "Lovish":67,
+    "Rohan":undefined
+}
+console.log(item["Harry"]);
