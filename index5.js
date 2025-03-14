ea227ccddf7387b5085dde6c55c593f1474aa123
ugaleabhi <ugaleabhishek34@gmail.com>
2025-03-14 19:59:03 +0530

    add file

---
+++ index5.js
@@ -0,0 +1,19 @@
+let a="Abhi"
+let b=6;
+console.log(a+b);
+console.log(typeof (a+b))
+const z={
+    name:"abhi",
+    Selection:1,
+    isPrincipal:false
+
+}
+z['friend']="Shubham"
+z['name']="Lovish"
+console.log(z);
+const dist={
+    quiche:"a savory pie filled with custard, cheese, and other ingredients",
+    hyaline:"glassy or transparent",
+    carp:"to find fault or complain unreasonably"
+}
+console.log(dist['carp'])
