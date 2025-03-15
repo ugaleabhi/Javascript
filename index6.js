a9ca340ccf1849caa957d0483bdb4febf333b27c
ugaleabhi <ugaleabhishek34@gmail.com>
2025-03-15 14:33:30 +0530

    add file

---
+++ index6.js
@@ -0,0 +1,28 @@
+console.log("Oprator in JS");
+//*************************************Arithmatic operator***********************************************
+let a =45;
+let b=4;
+// console.log("a+b=",a+b)
+// console.log("a-b=",a-b)
+// console.log("a/b=",a/b)
+// console.log("a*b=",a*b)
+// console.log("a**b=",a**b)
+// console.log("a%b=",a%b)
+// console.log("a++=",a++)
+// console.log("a--=",a--)
+//************************Assignment operator******************************************
+let assignment=1;
+assignment+=8;//same as assignment=assignment+5;
+console.log(assignment);
+//*************************comparison operator****************************************
+let comparison=6;
+let comparison2=7;
+console.log("comparison==comparsion2",comparison==comparison2);
+console.log("comparison==comparsion2",comparison!=comparison2);
+console.log("comparison===comparsion2",comparison===comparison2);
+console.log("comparison!==comparsion2",comparison!==comparison2);
+console.log("comparison>comparsion2",comparison>comparison2);
+//**********************logical oprator***********************************
+let x=5;
+let y=6;
+console.log(x<y&& x==5);
