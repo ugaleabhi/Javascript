dfd1d079c64c52f332def7615f5ccd538ad77302
ugaleabhi <ugaleabhishek34@gmail.com>
2025-03-24 12:23:10 +0530

    update file

--- index10.js
+++ index10.js
@@ -1,1 +1,14 @@
-//java scirpt
+// let n=prompt("Enter the value n:")
+// n=Number.parseInt(n)
+// let i=0;
+// while(i<n){
+//     console.log(i)
+//     i++;
+// }
+let n=prompt("Enter the value n:")
+ n=Number.parseInt(n)
+ let i=0;
+do{
+    console.log(i)
+     i++;
+ } while(i<n)
