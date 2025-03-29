7a639282de0b9bbd1178731f25128638548d2b96
ugaleabhi <ugaleabhishek34@gmail.com>
2025-03-29 11:37:42 +0530

    add file

---
+++ index13.js
@@ -0,0 +1,16 @@
+
+let name="Harry"
+console.log(name.length);
+let friend='Abhi'
+console.log(friend);
+
+//Template litral
+let boy1 ="Pramod"
+let boy2="Nikhil"
+let sentence=`${boy1} is friend ${boy2}`
+console.log(sentence);
+//Escape sequence character
+let fruit='Bana\'na'
+console.log(fruit);
+let fruit1="Bana\"na"
+console.log(fruit1);
