b78d8de3d9e00c068b14314ed7bd445f414f4dc8
ugaleabhi <ugaleabhishek34@gmail.com>
2025-03-30 18:01:57 +0530

    Add file

---
+++ index14.js
@@ -0,0 +1,17 @@
+let name="Abhi"
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.slice(2,4));
+// console.log(name.slice(2));
+// console.log(name.replace("Abh","Don"));
+// let friend="Naman"
+// console.log(name.concat(" a friend of ",friend));
+// let friend2= "        Meena         "
+// console.log(friend2.trim())
+let fr="Shivika"+" Abhi";
+console.log(fr[0]);
+console.log(fr[1]);
+console.log(fr[2]);
+console.log(fr[3]);
+console.log(fr);
